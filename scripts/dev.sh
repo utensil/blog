cb547f36b1b9212a0a2bee20ac117530d46dde83
utensil <utensilcandel@gmail.com>
2024-05-23 16:22:40 +0800

    Make plotting in Julia work

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -8,8 +8,8 @@ cd $PROJECT_ROOT
 # watchexec --ignore-nothing -vvv --project-origin . -w content/posts/math-2024 -e d2,pikchr,typ,bib --only-emit-events --emit-events-to json-stdio --print-events --notify --poll 500ms
 watchexec --project-origin . --on-busy-update queue --poll 500ms -e d2,pikchr,typ,bib --emit-events-to=stdio -- ./scripts/make_changed.sh &
 
-quarto preview --no-serve --no-browser --render all &
+quarto preview --execute-debug --no-serve --no-browser --render all &
 
-hugo serve -w --forceSyncStatic --disableFastRender --ignoreCache --noHTTPCache & # --gc --cleanDestinationDir
+watchexec --project-origin . --restart --poll 500ms -e qmd -- hugo serve -w --forceSyncStatic --disableFastRender --ignoreCache --noHTTPCache & # --gc --cleanDestinationDir
 
 wait
